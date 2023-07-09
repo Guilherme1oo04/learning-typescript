56d49145d3fd3f9225327a03f769997be7db7159
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2023-07-09 15:13:21 -0300

    Update in objetos.ts

--- tipos-ts/objetos/objetos.js
+++ tipos-ts/objetos/objetos.js
@@ -14,3 +14,21 @@ const funcao = (pessoa) => {
     console.log(`${pessoa.nome} trabalha como ${pessoa.funcao}.`);
 };
 funcao({ nome: "Guilherme Chaves", funcao: "Desenvolvedor full-stack" });
+const linguagensPreferidas = (pessoa) => {
+    return `As lingaugens preferidas de ${pessoa.nome} são: ${pessoa.linguagens}.`;
+};
+console.log(linguagensPreferidas({ nome: "Guilherme", linguagens: "Python, JavaScript" }));
+const mostraEmail = (pessoa) => {
+    if (pessoa.email == undefined) {
+        console.log(`${pessoa.nome} não preencheu o e-mail!`);
+    }
+    else {
+        console.log(`${pessoa.nome} preencheu o e-mail!`);
+    }
+};
+console.log(" ");
+mostraEmail({ nome: "Guilherme" });
+mostraEmail({ nome: "Guilherme", email: "blabla@gmail.com" });
+const filho1 = { nome: "Guilherme", sobrenome: "Chaves", idade: 17 };
+console.log(" ");
+console.log(filho1);
--- tipos-ts/objetos/objetos.ts
+++ tipos-ts/objetos/objetos.ts
@@ -15,12 +15,60 @@ const bemVindo = (funcionario: {nome: string}) => {
 }
 bemVindo({nome: "Guilherme Chaves"});
 
+// Interfaces
 interface Pessoa {
     nome: string;
     funcao: string
 }
 
 const funcao = (pessoa: Pessoa) => {
-    console.log(`${pessoa.nome} trabalha como ${pessoa.funcao}.`)
+    console.log(`${pessoa.nome} trabalha como ${pessoa.funcao}.`);
 }
 funcao({nome: "Guilherme Chaves", funcao: "Desenvolvedor full-stack"});
+
+// Types
+type Pessoa2 = {
+    nome: string;
+    linguagens: string;
+}
+
+const linguagensPreferidas = (pessoa: Pessoa2) => {
+    return `As lingaugens preferidas de ${pessoa.nome} são: ${pessoa.linguagens}.`;
+}
+console.log(linguagensPreferidas({nome: "Guilherme", linguagens: "Python, JavaScript"}));
+
+// Valores opcionais em objetos
+interface Pessoa4 {
+    nome: string;
+    email?: string;
+}
+const mostraEmail = (pessoa: Pessoa4) => {
+    if (pessoa.email == undefined){
+        console.log(`${pessoa.nome} não preencheu o e-mail!`);
+    } else{
+        console.log(`${pessoa.nome} preencheu o e-mail!`);
+    }
+}
+
+console.log(" ");
+mostraEmail({nome: "Guilherme"});
+mostraEmail({nome: "Guilherme", email: "blabla@gmail.com"});
+
+// Readonly - usado para não permitir que uma propriedade seja reatribuída
+interface Pessoa5 {
+    nome: string;
+    readonly email: string;
+}
+
+// Heranças
+interface Pai {
+    sobrenome: string;
+}
+interface Filho extends Pai {
+    nome: string;
+    idade: number;
+}
+
+const filho1: Filho = {nome: "Guilherme", sobrenome: "Chaves", idade: 17};
+console.log(" ");
+console.log(filho1);
