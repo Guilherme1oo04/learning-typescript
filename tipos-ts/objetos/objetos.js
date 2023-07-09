fb69862cc2241b8a67ceeb0a972148fa9bae71f6
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2023-07-09 14:35:11 -0300

    Second commit

--- tipos-ts/objetos/objetos.js
+++ tipos-ts/objetos/objetos.js
@@ -1,7 +1,16 @@
 "use strict";
 // Objetos
-let pessoa;
-pessoa = { nome: 'Guilherme Chaves', idade: 17 };
-console.log(`Nome = ${pessoa.nome}`);
-console.log(`Idade = ${pessoa.idade}`);
+let pessoa1;
+pessoa1 = { nome: 'Guilherme Chaves', idade: 17 };
+console.log(`Nome = ${pessoa1.nome}`);
+console.log(`Idade = ${pessoa1.idade}`);
 console.log(" ");
+// Funções usando objetos como parâmetros
+const bemVindo = (funcionario) => {
+    console.log(`Bem Vindo ${funcionario.nome}!`);
+};
+bemVindo({ nome: "Guilherme Chaves" });
+const funcao = (pessoa) => {
+    console.log(`${pessoa.nome} trabalha como ${pessoa.funcao}.`);
+};
+funcao({ nome: "Guilherme Chaves", funcao: "Desenvolvedor full-stack" });
--- tipos-ts/objetos/objetos.ts
+++ tipos-ts/objetos/objetos.ts
@@ -1,10 +1,26 @@
 // Objetos
-let pessoa: {
+let pessoa1: {
     nome: string;
     idade: number
 };
 
-pessoa = {nome: 'Guilherme Chaves', idade: 17};
-console.log(`Nome = ${pessoa.nome}`);
-console.log(`Idade = ${pessoa.idade}`);
+pessoa1 = {nome: 'Guilherme Chaves', idade: 17};
+console.log(`Nome = ${pessoa1.nome}`);
+console.log(`Idade = ${pessoa1.idade}`);
 console.log(" ");
+
+// Funções usando objetos como parâmetros
+const bemVindo = (funcionario: {nome: string}) => {
+    console.log(`Bem Vindo ${funcionario.nome}!`)
+}
+bemVindo({nome: "Guilherme Chaves"});
+
+interface Pessoa {
+    nome: string;
+    funcao: string
+}
+
+const funcao = (pessoa: Pessoa) => {
+    console.log(`${pessoa.nome} trabalha como ${pessoa.funcao}.`)
+}
+funcao({nome: "Guilherme Chaves", funcao: "Desenvolvedor full-stack"});
