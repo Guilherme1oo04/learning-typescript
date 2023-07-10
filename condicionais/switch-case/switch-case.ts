eae40200a0808e90f722a9f1af213561e4b0eec5
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2023-07-10 09:58:19 -0300

    Adicionando Switch Case

---
+++ condicionais/switch-case/switch-case.ts
@@ -0,0 +1,28 @@
+const flores = (flor: string) => {
+    let flor1 = flor.toLowerCase();
+    
+    if (flor1[flor1.length - 1] != 's'){
+        flor1 = flor1 + "s";
+    }
+
+    switch (flor1) {
+        case 'tulipas':
+            console.log("Tulipas são brancas!");
+            break;
+        
+        case 'rosas':
+            console.log("Rosas são vermelhas!");
+            break;
+
+        case 'violetas':
+            console.log("Violetas são azuis!");
+            break;
+    
+        default:
+            console.log("Essa flor não está no banco de dados!")
+            break;
+    }
+}
+
+flores("tulipas");
+flores("tulipa");
